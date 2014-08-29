8f0de8e392b916f189a8f1bb40f6b7f741e1128c
HackFisher <hackfisher@gmail.com>
2014-08-29 01:44:33 -0400

    update script

--- utils/restart_on_fail.sh
+++ utils/restart_on_fail.sh
@@ -1,6 +1,6 @@
 while [ 1=1 ] ; do
  echo "Starting node server" >>node1.log
- ./bitshares_client --data-dir=seed1 --p2p-port=1776 --input-log=command.txt
+ ./bitshares_client --data-dir=seed1 --p2p-port=1776 --input-log=./command.txt
  echo "Balls, crashed" >> node1.log
  sleep 5
 done
