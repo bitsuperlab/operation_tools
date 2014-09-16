b5b0bdaceca413b38f94b805150cce5fdea145a3
alt <pch957@163.com>
2014-09-16 10:44:15 +0800

    support parameter "ALL", this will publish all asset

--- btsxfeed/btsx_feed_auto.py
+++ btsxfeed/btsx_feed_auto.py
@@ -28,10 +28,14 @@ max_update_hours = config["price_limit"]["max_update_hours"]
 sample_timer = config["price_limit"]["sample_timer"]
 median_length = config["price_limit"]["median_length"]
 
+asset_list_all = ["PTS", "PPC", "LTC", "BTC", "WTI", "SLV", "GLD", "TRY", "SGD", "HKD", "RUB", "SEK", "NZD", "CNY", "MXN", "CAD", "CHF", "AUD", "GBP", "JPY", "EUR", "USD"]
+if sys.argv[1] == "ALL":
+  asset_list_publish = asset_list_all
+  asset_list_display = asset_list_all
+else:
   asset_list_publish = sys.argv
   asset_list_publish.pop(0)
   asset_list_display = list(set(config["asset_list_display"] + asset_list_publish))
-asset_list_all = ["PTS", "PPC", "LTC", "BTC", "WTI", "SLV", "GLD", "TRY", "SGD", "HKD", "RUB", "SEK", "NZD", "CNY", "MXN", "CAD", "CHF", "AUD", "GBP", "JPY", "EUR", "USD"]
 
 delegate_list = config["delegate_list"]
 rate_cny = {}
